c0a6688a8b07b6258762723be6d3786a3a8ab49c
Henry Noon <henrynoon@icloud.com>
2020-03-01 13:51:47 +0000

    Create grid of UIViews

--- GestureRecognizerFun/ViewController.swift
+++ GestureRecognizerFun/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        
+        let numOfSquaresPerRow = 10
+        
+        let width = Int(view.frame.width)
+        let squareWidth = width/numOfSquaresPerRow
+        
+        let height = Int(view.frame.height)
+        
+        let numOfVerticals = height/squareWidth
+        
+        for i in 0...numOfSquaresPerRow {
+            for j in 0...numOfVerticals {
+                let square = UIView()
+                square.backgroundColor = randomColor()
+                square.layer.borderWidth = 0.5
+                square.layer.borderColor = UIColor.black.cgColor
+                square.frame = CGRect(x: i*squareWidth, y: j*squareWidth, width: squareWidth, height: squareWidth)
+                view.addSubview(square)
+            }
+        }
     }
-
-
+ 
+    fileprivate func randomColor() -> UIColor {
+        let redRandom = CGFloat(drand48())
+        let greenRandom = CGFloat(drand48())
+        let blueRandom = CGFloat(drand48())
+        
+        return UIColor(displayP3Red: redRandom, green: greenRandom, blue: blueRandom, alpha: 1)
+    }
 }
 
